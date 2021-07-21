b48f4e54ee8616a6c88c8b7b8d61a0b1e0df8778
Aditya Kashyap <aditya.kashyap@testinium.com>
2021-07-21 15:27:46 +0530

    "Removed Not Required Files And added Intro Screen"

--- lib/Screens/Splash.dart
+++ lib/Screens/Splash.dart
@@ -21,25 +21,7 @@ class _SplashState extends State<Splash> {
     await Provider.of<MainProvider>(context, listen: false).decideSplash();
   }
 
-  final List<Map<String, String>> _splashList = [
-    {
-      "title": "Start Learning",
-      "text":
-      "Start learning now by using this app, Get your choosen course and start the journey.",
-      "image": "assets/fb.png",
-    },
-    {
-      "title": "Explore Courses",
-      "text": "Choose which course is suitable for you to enroll in.",
-      "image": "asset/fb.png",
-    },
-    {
-      "title": "At Any time.",
-      "text": "Your courses is available at any time you want. Join us now !",
-      "image": "asset/fb.png"
-    },
-  ];
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -49,66 +31,8 @@ class _SplashState extends State<Splash> {
         // primarySwatch: Color(0xff01b4c9),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CurvedSplashScreen(
-          screensLength: _splashList.length,
-          screenBuilder: (index) {
-            return SplashContent(
-              key: Key(_splashList[index]["title"]!),
-              title: _splashList[index]["title"]!,
-              image: _splashList[index]["image"]!,
-              text: _splashList[index]["text"]!,
-            );
-          },
-          onSkipButton: () {
-            print("Ends");
-          }),
+      home: Splash()
     );
   }
 }
 
-class SplashContent extends StatelessWidget {
-  final String title;
-  final String text;
-  final String image;
-
-  const SplashContent({
-    required Key key,
-    required this.title,
-    required this.text,
-    required this.image,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 15.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 100),
-          Container(
-            height: 200,
-            child: Image.asset(image),
-          ),
-          SizedBox(height: 60),
-          Text(
-            title,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 27,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            text,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 19,
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hookup4u2/main.dart';
+import 'package:cns/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
